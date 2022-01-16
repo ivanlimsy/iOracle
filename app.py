f73597db7e451cf29227b805cf3e6a734317b803
Derrick Wong <darkwing2025@gmail.com>
2022-01-16 14:37:12 +0800

    streamlit partially done

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 from datetime import date
+import requests
 
 
 
@@ -16,11 +17,20 @@ side.markdown('## Choose the stock and the number of days to predict')
 stock = side.selectbox("Stock", ("Apple",))
 # num_days = side.slider(f"Number of Days from {date.today()}", 1, 14, value=5, step =1)
 
+# can add more in future
+ticker_dict  = {'Apple': 'aapl'}
+
 # get api
-api_url = ""
+api_url = "https://rfpred-dh3l3t4ama-ew.a.run.app/predict"
 
 ### might need to change 
-params = {"Stock": stock}
+params = {"ticker_name": ticker_dict.get(stock, 0)}
+
+response = requests.get(
+    api_url,
+    params=params).json()
+
+st.write(response['prediction'])
 
 
 
